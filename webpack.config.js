7e56706a3a5722fb52b1e0d71e45e029658f371d
Ron-Sungcang <sungcangjoseph@gmail.com>
2025-01-23 22:59:32 -0600

    added HtmlWebpackPlugin to make index.html the template

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -9,13 +11,34 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+
+    // Module rules to deal with different type of files
+    module: {
+        rules: [
+            // Handling CSS files
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+
     plugins: [
+        // Clean the output directory before each build
+        new CleanWebpackPlugin(),
+
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html'
+        }),
+
         new CopyWebpackPlugin({
             patterns:[
-                {from: 'index.html', to: 'index.html'},
                 {from: 'styles.css', to: 'styles.css'},
                 {from: 'manifest.json', to: 'manifest.json'},
-                {from: 'icons/', to: 'icons/'}
+                {from: 'icons/', to: 'icons/'},
+                {from: 'service-worker.js', to: 'service-worker.js'},
+                {from: 'script.js', to: 'script.js'}
             ],
         }),
     ],
