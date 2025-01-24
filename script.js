43f41dbf04cd5f80707be434a7786e8ed37f6766
Tanner L <tdroque@gmail.com>
2025-01-23 18:16:42 -0600

    Inital commit, setup code files, PWA, and readme

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Placeholder for script file, just loads time, but ensures file is imported correctly
+document.getElementById("text").textContent = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();
+
+
