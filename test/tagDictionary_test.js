304f55eafeb4c60f7e511552c0cb686fb693cb94
Tanner L <tdroque@gmail.com>
2025-01-30 10:52:01 -0600

    tag dictionary tests added, test found tag and unknown tag

---
+++ test/tagDictionary_test.js
@@ -0,0 +1,11 @@
+import { TagDictionary } from "../tagDictionary/dictionary.js"
+
+QUnit.module("Tag Dictionary")
+
+QUnit.test("Tag Dict test", (assert) => {
+    const tagDict = new TagDictionary()
+
+    assert.equal(tagDict.lookup("X00080005"), "SpecificCharacterSet", "correct tag returned")
+    assert.equal(tagDict.lookup("X000000"), "Unknown", "Unknown tag returned")
+    
+})
