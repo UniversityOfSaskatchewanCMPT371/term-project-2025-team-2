412c26ddcd3bc9560a55e8992fa416a11e156ec6
Ron-Sungcang <Ron-Sungcang@users.noreply.github.com>
2025-01-30 02:28:51 +0000

    deploy: 9ec1b45f8755c34337754c8c3669116c30a7f5b3

--- logger/logger.js
+++ logger/logger.js
@@ -1,20 +1,100 @@
 
-// This function logs a message to the database
-export function logMessage(level, message) {
 
-  fetch('https://us-central1-data-a9e6d.cloudfunctions.net/app/entry', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        title: level,
-        text: message
-    })
-  })
-  .then(response => response.json())
-  .catch(error => console.error(error));
-
+// check if running in broswer or node
+let userAgent;
+if (typeof document != "undefined") {
+  userAgent = window.navigator.userAgent;
 }
 
+/**
+ * Logger class to handle logging messages to a file and to a remote server
+ * @class
+ */
+export class Logger {
+
+  /**
+   * Constructor for the Logger class
+   * @constructor
+   * @param {string} handle - The file handle to write log messages to
+   * @param {string} postUrl - The URL to post log messages to
+   */
+  constructor(handle, postUrl) {
+    this.handle = handle || null;
+    this.postUrl = postUrl || 'https://us-central1-data-a9e6d.cloudfunctions.net/app/add/msg';
+  }
+
+  /**
+   * Set the file handle to write log messages to
+   */
+  async pickHandle() {
+    this.handle = await window.showSaveFilePicker();
+    this.writableStream = await this.handle.createWritable();
+  }
+
+  /**
+   * Write a log message to the local file
+   * @param {string} level - The log level of the message
+   * @param {string} message - The message to log
+   * @returns {Promise} - A promise that resolves when the message is written
+   */
+  async localLog(level, message) {
+    if (this.handle == null) {
+      console.log("No local log file set");
+    } else {
+      try {
+        await this.writableStream.write(JSON.stringify(`{${level}: ${message}}`) + ",\n");
+      } catch (err) {
+        console.error(err.name, err.message);
+      }
+    }
+  }
+
+  /**
+   * Close the local log file
+   * @returns {Promise} - A promise that resolves when the file is closed
+   */
+  async closeFile() {
+    await this.writableStream.close();
+    console.log("close log file");
+  }
+
+  /**
+   * Log a message to the local file and to the remote server
+   * @param {string} level - The log level of the message
+   * @param {string} message - The message to log
+   */
+  log(level, message) {
+    this.localLog(level, message);
+    this.logMessage(level, message);
+  }
+
+  /**
+   * Post a log message to the remote server
+   * @param {string} level - The log level of the message
+   * @param {string} message - The message to log
+   * @returns {Promise} - A promise that resolves when the message is posted
+   */
+  async logMessage(level, message) {
+    await fetch(this.postUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        level: level,
+        msg: message,
+        userAgent: userAgent
+      })
+    }).then(response => {
+      if (response.ok) {
+        return { status: "success" };
+      } else {
+        return { status: "error" };
+      }
+    }).catch(error => {
+      console.error('Error:', error);
+    });
+  }
+
+}
 
