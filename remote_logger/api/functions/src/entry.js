f7ae63d3e06427a48362d8cdbc19a962114e0103
Tanner L <tdroque@gmail.com>
2025-01-30 13:28:58 -0600

    add remote web log message storage code

---
+++ remote_logger/api/functions/src/entry.js
@@ -0,0 +1,41 @@
+const {db} = require("../config/firebase_config.js");
+const requestIp = require("request-ip");
+
+const addLog = async (req, res) => {
+  const {level, msg, userAgent} = req.body;
+
+  try {
+    const log = db.collection("logs").doc();
+    const logObject = {
+      id: log.id,
+      level,
+      msg,
+      userAgent,
+      date: new Date().toISOString(),
+      ip: requestIp.getClientIp(req),
+    };
+
+    log.set(logObject);
+
+    res.status(200).send({
+      status: "success",
+      message: "entry added successfully",
+      data: logObject,
+    });
+  } catch (error) {
+    res.status(500).json("We found an error posting your request!");
+  }
+};
+
+// const getAllEntries = async (req, res) => {
+//   try {
+//     const allEntries = [];
+//     const querySnapshot = await db.collection("entries").get();
+//     querySnapshot.forEach((doc) => allEntries.push(doc.data()));
+//     return res.status(200).json(allEntries);
+//   } catch (error) {
+//     return res.status(500).json("We found an error fetching your request!");
+//   }
+// };
+
+module.exports = {addLog};
---
+++ remote_logger/readme.md
@@ -0,0 +1,13 @@
+#Remote Log Storage
+
+## Logging API
+
+* Endpoint for centralized log message storage
+* Is a Firebase function
+
+## Logging Web Portal
+
+* Web UI to view all stored log messages
+* Uses Firebase Hosting
+
+
