9673685b2d3601ae78f64c98457a6e3d6d42c66e
Tanner L <tdroque@gmail.com>
2025-02-04 21:02:09 -0600

    add to eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,10 @@ export default [
             },
         },
     },
+    { 
+        ignores: ["dicomParser/*", "tagEditor/*"] 
+    },
+
     pluginJs.configs.recommended,
     eslintConfigPrettier,
 ]
