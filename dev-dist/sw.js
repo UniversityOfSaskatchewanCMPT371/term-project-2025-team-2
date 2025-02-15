5a38eaafcdd52acf0855d7e88e86276a1cf7e172
baolin yang <baoliny961@gmail.com>
2025-02-15 15:50:44 -0600

    add PWA development files in gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -89,7 +89,7 @@ define(['./workbox-3c498241'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.6be24665778"
+    "revision": "0.gdcbd7fl90g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
