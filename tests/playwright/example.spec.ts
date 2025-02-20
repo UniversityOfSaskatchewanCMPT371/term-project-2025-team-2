e78a3e8c040c992d302dce81b7982f69d94127e1
MohammadHafiz Kapadwala <wbj741@usask.ca>
2025-02-20 17:27:01 -0600

    Completed view dicom tags for the uploaded file

--- tests/playwright/example.spec.ts
+++ tests/playwright/example.spec.ts
@@ -21,7 +21,7 @@
 // });
 
 import { test, expect } from '@playwright/test';
-test('Upload DICOM file and verify it appears in the list', async ({ page }) => {
+test('Upload DICOM file ', async ({ page }) => {
     // Navigate to your DICOM tag editor
     await page.goto(' http://localhost:5173');
 
@@ -29,4 +29,21 @@ test('Upload DICOM file and verify it appears in the list', async ({ page }) => 
     const fileInput = page.locator('input[type="file"].hidden');
     await fileInput.setInputFiles("./test-data/000000.dcm");
     console.log('File uploaded');
+});
+
+test('View DICOM tags for an uploaded file', async ({ page }) => {
+    await page.goto('http://localhost:5173');
+
+    // Upload a DICOM file
+    const fileInput = page.locator('input[type="file"].hidden');
+    await fileInput.setInputFiles('./test-data/CR000001.dcm');
+    await page.waitForTimeout(1000);
+
+    // Verify that the DICOM tags table is displayed
+    const tagTable = page.locator('table');
+    await expect(tagTable).toBeVisible();
+
+    const sopClassUID = page.locator('tr', { has: page.locator('td', { hasText: "SOPClassUID" }) }).first();
+    await expect(sopClassUID).toBeVisible();
+    console.log("Dicom tags for the uploaded file is visible")
 });
