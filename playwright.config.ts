2f58915431bd08204a363c4c5e4c428f7a5ed7c2
MohammadHafiz Kapadwala <wbj741@usask.ca>
2025-02-21 15:24:31 -0600

    Updated the config file again

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     webServer: {
         command: 'npm run dev',
         port: 5173,
+        reuseExistingServer: !process.env.CI, // Avoids duplicate servers in local runs
+        timeout: 120000,
     },
     use: {
         baseURL: 'http://localhost:5173',
