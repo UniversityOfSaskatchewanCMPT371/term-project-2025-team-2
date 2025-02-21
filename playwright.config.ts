3c7be2006104729d93e7cebef2be209506df110d
MohammadHafiz Kapadwala <wbj741@usask.ca>
2025-02-21 13:21:49 -0600

    Updated playwright config file to run the server before running the playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,15 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
     testDir: "./tests/playwright",
     timeout: 3000,
+    webServer: {
+        command: 'npm run dev',
+        port: 5173,
+        reuseExistingServer: !process.env.CI, // Avoids duplicate servers in local runs
+    },
     use: {
+        baseURL: 'http://localhost:5173',
         headless: true,
         browserName: "firefox",
     },
+
 });
