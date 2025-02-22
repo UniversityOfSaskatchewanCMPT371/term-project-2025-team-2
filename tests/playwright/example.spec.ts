91559338b477233c776db6e6ee87d71a1a28a55b
Tanner <tdroque@gmail.com>
2025-02-22 11:24:30 -0600

    fix playwright test

--- tests/playwright/example.spec.ts
+++ tests/playwright/example.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from "@playwright/test";
 test.describe.configure({ mode: "parallel" });
 test("Upload DICOM file ", async ({ page }) => {
+    test.setTimeout(60000);
     // Navigate to your DICOM tag editor
     await page.goto(" http://localhost:5173");
 
     // Upload a DICOM file
     const fileInput = page.locator('input[type="file"].hidden');
     await fileInput.setInputFiles("./test-data/000000.dcm");
+    test.setTimeout(60000);
     console.log("File uploaded");
 });
 
