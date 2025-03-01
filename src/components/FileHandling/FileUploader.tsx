eda02cb3a22fcb615d2c693a4e6de858fa99f4bd
Tanner <tdroque@gmail.com>
2025-03-01 11:44:07 -0600

    check file extensions

--- src/components/FileHandling/FileUploader.tsx
+++ src/components/FileHandling/FileUploader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDropzone } from "react-dropzone";
 import { parseDicomFile } from "../DicomData/DicomParserUtils.ts";
 import { FileUploaderProps } from "../../types/FileTypes.ts";
-import logger from "../utils/Logger.tsx";
+import logger from "../utils/Logger";
 
 /**
  *
