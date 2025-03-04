06e3bcce150fcae266b9f626ae9f6ea25b0a895d
Tanner <tdroque@gmail.com>
2025-03-04 08:31:43 -0600

    auto anon one tag at a time error check

--- src/components/Auto/AutoClean.ts
+++ src/components/Auto/AutoClean.ts
@@ -1,8 +1,7 @@
-import { Test_TagsAnon } from "./TagsAnon";
+import { TagsAnon } from "./TagsAnon";
 import { tagUpdater } from "../DicomData/TagUpdater";
 import {
     createFile,
-    createZipFromFiles,
     downloadDicomFile,
 } from "../DicomData/DownloadFuncs";
 import { CustomFile } from "../../types/FileTypes";
@@ -12,28 +11,28 @@ import { CustomFile } from "../../types/FileTypes";
  * @param dicomData
  * @returns formatted data
  */
-export function FormatData(dicomData: any) {
+export function FormatData(dicomData: any, tag: any) {
     const newDicomData: any = [];
 
-    Test_TagsAnon.forEach((tag: any) => {
-        if (!dicomData.DicomDataSet.elements[tag.tagId.toLowerCase()]) {
-            return;
-        }
+    // tags.forEach((tag: any) => {
+    if (!dicomData.DicomDataSet.elements[tag.tagId.toLowerCase()]) {
+        return;
+    }
 
-        const tagData = {
-            tagId: tag.tagId,
-            newValue: tag.value,
-            vr:
-                dicomData.DicomDataSet.elements[tag.tagId.toLowerCase()].vr ||
-                "UN",
-            dataOffSet:
-                dicomData.DicomDataSet.elements[tag.tagId.toLowerCase()]
-                    .dataOffset || 0,
-            length: tag.value.length,
-            deleteTag: false,
-        };
-        newDicomData.push(tagData);
-    });
+    const tagData = {
+        tagId: tag.tagId,
+        newValue: tag.value,
+        vr:
+            dicomData.DicomDataSet.elements[tag.tagId.toLowerCase()].vr ||
+            "NO",
+        dataOffSet:
+            dicomData.DicomDataSet.elements[tag.tagId.toLowerCase()]
+                .dataOffset,
+        length: tag.value.length,
+        deleteTag: false,
+    };
+    newDicomData.push(tagData);
+    // });
 
     return newDicomData;
 }
@@ -46,15 +45,29 @@ export function FormatData(dicomData: any) {
  * @returns none
  */
 export const AutoAnon = async (dicomData: any[], files: CustomFile[]) => {
-    const newFiles: any = [];
-    const formatedData = FormatData(dicomData[0]);
+    // const newFiles: any = [];
 
-    dicomData.forEach((dicom: any, index: number) => {
-        const updatedFile = tagUpdater(dicom.DicomDataSet, formatedData);
+    TagsAnon.forEach((tag: any) => {
+        
+        // console.log("tag", tag);
+        
+        const formatedData = FormatData(dicomData[0], tag);
+        let newFile: any;
+        
+       
+        if(!formatedData) {
+            return;
+        }
+        console.log("tag update ", formatedData);
+        // dicomData.forEach((dicom: any, index: number) => {
+        const updatedFile = tagUpdater(dicomData[0].DicomDataSet, formatedData);
 
-        newFiles.push(createFile(files[index].name, updatedFile));
+        newFile = createFile(tag.tagId + "_" + formatedData[0].vr, updatedFile);
+        // newFiles.push(createFile(files[index].name, updatedFile));
+        // });
+
+        downloadDicomFile(newFile);
     });
-
-    const zipFile = await createZipFromFiles(newFiles);
-    downloadDicomFile({ name: "updateDicoms.zip", content: zipFile });
+    // const zipFile = await createZipFromFiles(newFiles);
+    // downloadDicomFile({ name: "updateDicoms.zip", content: zipFile });
 };
