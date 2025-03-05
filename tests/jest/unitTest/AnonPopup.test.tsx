9de4ad6cf8973c20e4d3a544bd8cdb77bfe333d1
Aladin <aaladino581@gmail.com>
2025-03-04 18:40:55 -0600

    added test to confirm contents render properly in AnonPopup

--- tests/jest/unitTest/AnonPopup.test.tsx
+++ tests/jest/unitTest/AnonPopup.test.tsx
@@ -2,8 +2,8 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import AnonPopup from '../../../src/components/DicomData/TableComponents/AnonPopup';
 
 const mockTags = [
-    { tagId: '0010,0010', tagName: 'PatientName', newValue: 'John Doe' },
-    { tagId: '0010,0020', tagName: 'PatientID', newValue: '123456' },
+    { tagId: 'X00100010', tagName: 'PatientName', newValue: 'John Doe' },
+    { tagId: 'X00100020', tagName: 'PatientID', newValue: '123456' },
 ];
 
 describe('AnonPopup', () => {
@@ -22,4 +22,16 @@ describe('AnonPopup', () => {
         fireEvent.click(screen.getByText('Cancel'));
         expect(onCancel).toHaveBeenCalledTimes(1);
     });
+
+    test('renders the popup with tags', () => {
+        render(<AnonPopup tags={mockTags} onConfirm={jest.fn()} onCancel={jest.fn()} />);
+
+        expect(screen.getByText('Tags to be Anonymized')).toBeInTheDocument();
+
+        // checks list items (li elements) in popup
+        const listItems = screen.getAllByRole("listitem");
+        expect(listItems[0]).toHaveTextContent("X00100010 (PatientName): John Doe");
+        expect(listItems[1]).toHaveTextContent("X00100020 (PatientID): 123456");
+    });
+
 });
