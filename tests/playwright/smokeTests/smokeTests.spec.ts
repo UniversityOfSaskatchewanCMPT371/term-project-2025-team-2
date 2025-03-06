e80761e552f39a7a1977aa405b1492fc74715912
Ron-Sungcang <sungcangjoseph@gmail.com>
2025-03-05 19:27:21 -0600

    Added smoke test to workflow

--- tests/playwright/smokeTests/smokeTests.spec.ts
+++ tests/playwright/smokeTests/smokeTests.spec.ts
@@ -40,12 +40,12 @@ test("Upload dicom and verify file name matches tag displayed", async ({
 
         await page.waitForSelector("text=/Currently Viewing: .+\.dcm/", {
             state: "visible",
-            timeout: 1000,
+            timeout:  5000,
         });
 
         await page.waitForSelector("table", {
             state: "visible",
-            timeout: 1000,
+            timeout: 5000,
         });
 
         if (DEBUG) {
