4bf3082f10e805043a4f34466e1c02eef2bd7136
Aladin <aaladino581@gmail.com>
2025-03-08 12:06:37 -0600

    test onSearchChange called with valid input when search is made

--- tests/jest/unitTest/UI_layer/TableControls.test.tsx
+++ tests/jest/unitTest/UI_layer/TableControls.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TableControls from '../../../../src/components/DicomData/TableComponents/TableControls';
 
 describe('TableControls', () => {
@@ -6,6 +6,14 @@ describe('TableControls', () => {
         render(<TableControls searchTerm="" onSearchChange={jest.fn()} onSave={jest.fn()} />);
         expect(screen.getByText('Download File')).toBeInTheDocument();
         expect(screen.getByText('Auto Anon')).toBeInTheDocument();
+    });
+
+    test('calls onSearchChange when search term is changed', () => {
+        const onSearchChange = jest.fn();
+        render(<TableControls searchTerm="" onSearchChange={onSearchChange} onSave={jest.fn()} />);
+        const searchInput = screen.getByRole('textbox');
+        fireEvent.change(searchInput, { target: { value: 'test' } });
+        expect(onSearchChange).toHaveBeenCalledWith('test');
     });
 
 });
