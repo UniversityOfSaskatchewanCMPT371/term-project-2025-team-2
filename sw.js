95d36be5a9884cea0c51f14a0034fbe115d7ee14
baolinyang1 <baolinyang1@users.noreply.github.com>
2025-03-17 21:49:38 +0000

    deploy: e80ca4411c5bf4b364f2aeec08fa6af803df97bf

--- sw.js
+++ sw.js
@@ -91,10 +91,10 @@ define(['./workbox-3c498241'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon.png",
     "revision": "c84a151eb988fc8574c09a2eb4697489"
   }, {
-    "url": "assets/index--R4lh0Mu.js",
+    "url": "assets/index-BCfLsMF6.js",
     "revision": null
   }, {
-    "url": "assets/index-BdLOSV4b.css",
+    "url": "assets/index-Dp38wV-K.css",
     "revision": null
   }, {
     "url": "assets/workbox-window.prod.es5-B9K5rw8f.js",
@@ -107,7 +107,7 @@ define(['./workbox-3c498241'], (function (workbox) { 'use strict';
     "revision": "22479316f56895e8dd84af4c1619e1b2"
   }, {
     "url": "index.html",
-    "revision": "44d96777b92a56e87509ec91073c2ff9"
+    "revision": "a481b2a2d69059fee8b0d1a9df9305f3"
   }, {
     "url": "maskable_icon.png",
     "revision": "ae68a3429b51f884211cebf8011bc4eb"
