5a5f2b41297f149d1c2f25763c49e9d993973759
baolin yang <baoliny961@gmail.com>
2025-03-17 20:11:30 -0600

    re-format

--- src/Hooks/useFilteredRows.ts
+++ src/Hooks/useFilteredRows.ts
@@ -1,6 +1,15 @@
 import { useMemo } from "react";
 import logger from "@logger/Logger";
 
+/**
+ * Custom hook for filtering DICOM table rows
+ * @function
+ * @precondition useFilteredRows hook expects the following parameters
+ * @postcondition Returns filtered array of table rows
+ * @param {TableRow[]} rows - Array of table rows to filter
+ * @param {string} searchTerm - Current search term
+ * @returns {TableRow[]} Filtered array of table rows
+ */
 export const useFilteredRows = (rows: any[], searchTerm: string) => {
     logger.debug(`Filtering rows with search term: ${searchTerm}`);
     logger.debug(`Rows length: ${rows.length}`);
