37c26ca2553aed44f5a2d167032f442c22e34da0
Aladin-019 <Aladin-019@users.noreply.github.com>
2025-03-20 23:13:43 +0000

    deploy: 3d7f3af96711461ef6e7fd68c5439847c8ab9492

--- sw.js
+++ sw.js
@@ -91,10 +91,10 @@ define(['./workbox-3c498241'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon.png",
     "revision": "c84a151eb988fc8574c09a2eb4697489"
   }, {
-    "url": "assets/index-CkpMGJpG.css",
+    "url": "assets/index-B4GIEVS-.js",
     "revision": null
   }, {
-    "url": "assets/index-WSEwq8MN.js",
+    "url": "assets/index-CkpMGJpG.css",
     "revision": null
   }, {
     "url": "assets/workbox-window.prod.es5-B9K5rw8f.js",
@@ -107,7 +107,7 @@ define(['./workbox-3c498241'], (function (workbox) { 'use strict';
     "revision": "22479316f56895e8dd84af4c1619e1b2"
   }, {
     "url": "index.html",
-    "revision": "4041027f2ecbef4135194a2615561505"
+    "revision": "f25827d3c6a99adbf17ab7918ae85f8f"
   }, {
     "url": "maskable_icon.png",
     "revision": "ae68a3429b51f884211cebf8011bc4eb"
